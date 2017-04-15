7eb53089a293647779f4dc9185d5406909c24092
Jose Cordero <jose.cordero.rama@gmail.com>
2017-04-15 19:43:38 +0200

    Huge refactoring

--- common/utils.py
+++
@@ -1,9 +0,0 @@
-from pylab import *
-
-def multiplot(images, shape):
-    figure()
-    for i in range(len(images)):
-        if i+1 > shape[0]*shape[1]:
-            break
-        subplot(shape[0], shape[1], 1+i)
-        imshow(images[i])
